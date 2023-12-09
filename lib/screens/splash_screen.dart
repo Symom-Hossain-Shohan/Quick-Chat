a0665da1c987c2b9f434536bc31887d1d09ebd6f
Symom-Hossain-Shohan <61089607+Symom-Hossain-Shohan@users.noreply.github.com>
2023-12-09 16:37:38 +0600

    xommnt

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -16,7 +16,7 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreen();
 }
 
-//comment
+
 
 class _SplashScreen extends State<SplashScreen> {
   void initState() {
